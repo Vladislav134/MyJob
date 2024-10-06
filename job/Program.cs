b8db92d63a5fc7eb3cf9a839f731fdafd9226c74
Vladislav134 <137281737+Vladislav134@users.noreply.github.com>
2024-10-06 13:41:24 +0300

    prelast step

--- job/Program.cs
+++ job/Program.cs
@@ -15,3 +15,22 @@ void PrintMatrix (string[] strArr)
     for (int i = 0; i < strArr.Length; i++)
     Console.Write($"\"{strArr[i]}\"  ");
 }
+
+string[] Filtr (int n, string[] strArr)
+{
+ string[] Arr = new string[n];
+ for (int i = 0; i < n; i++) 
+ {
+    if (strArr[i].Length < 4)
+    
+    Arr[i] = strArr[i];
+    
+ }
+  return Arr;
+ }
+
+ void PrintMatrix2 (string[] Arr)
+{
+    for (int i = 0; i < Arr.Length; i++)
+    Console.Write($"\"{Arr[i]}\"  ");
+}
