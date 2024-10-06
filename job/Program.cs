2ab9550700914580e61b5dc9736986d05e0b8180
Vladislav134 <137281737+Vladislav134@users.noreply.github.com>
2024-10-06 13:40:38 +0300

    new step

--- job/Program.cs
+++ job/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Data;
+using System.Numerics;
+string[] Matrix(int n)
+{
+    string[] strArr = new string[n];
+    for (int i = 0; i < n; i++)
+    {
+    strArr[i] = Console.ReadLine()!;
+    }
+    return strArr;
+}
+
+void PrintMatrix (string[] strArr)
+{
+    for (int i = 0; i < strArr.Length; i++)
+    Console.Write($"\"{strArr[i]}\"  ");
+}
